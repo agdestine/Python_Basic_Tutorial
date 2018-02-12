c6c21b7478dda5c4c825830ca24ceaa63b5e8158
Destine <agdestine@corp.brh.com>
2018-02-12 14:18:49 -0500

    Tutorial 1

---
+++ Tutorial_1/Exercice1A.py
@@ -0,0 +1,7 @@
+#Import the string library.
+import string;
+
+#Create a variable alphabet that consists of the lowercase and uppercase letters in the English alphabet 
+# using the ascii_letters data attribute of the string library.
+alphabet = string.ascii_letters;
+
